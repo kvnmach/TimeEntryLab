f233c8d0adeb7acd1a164ece7e5fd2bf49ee24b5
kvnmach <kvnmach@gmail.com>
2016-03-17 15:47:26 -0500

    moved all classes to developer

--- TimeEntryLab/Developer.cs
+++ TimeEntryLab/Developer.cs
@@ -11,4 +11,39 @@ namespace TimeEntryLab
         public DateTime Date { get; set; }
         public virtual ICollection<Developer>Developers { get; set; } 
     }
+
+    public class Client
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public virtual ICollection<Client> Clients { get; set; }
+    }
+
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public virtual ICollection<Project> Projects { get; set; }
+    }
+
+   public class Task
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Task> Tasks { get; set; }
+
+    }
+
+   public class DevelopmentGroup
+    {
+        public string Name { get; set; }
+        public virtual ICollection<DevelopmentGroup> DevelopmentGroups { get; set; }
+    }
+
+   public class Industry
+    {
+        public string Name { get; set; }
+
+    }
 }
